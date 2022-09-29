dbfba169fba00e361f1c0dd0a55dc9722a61b5a1
c-bata <contact@c-bata.link>
2022-09-29 11:37:26 +0900

    Use transactional DDL

--- reproduce_script.sh
+++ reproduce_script.sh
@@ -4,8 +4,11 @@ set -eux
 rm alembic.db || true
 
 alembic upgrade 90823bd3809e
+echo ".tables" | sqlite3 alembic.db
+echo ".schema foo" | sqlite3 alembic.db
 
 alembic upgrade head | true
-
 echo ".tables" | sqlite3 alembic.db
+echo ".schema foo" | sqlite3 alembic.db
+echo "select * from foo;" | sqlite3 alembic.db
 
